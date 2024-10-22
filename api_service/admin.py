ae064a93a43b3fcb35bbcccf634fd647bbbb4062
Pete <opletaevpetr@mail.ru>
2024-10-22 22:06:32 +0300

    Profile Model Implemented

--- api_service/admin.py
+++ api_service/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'is_tutor')  
+    search_fields = ('user__username',)
+
+admin.site.register(Profile, ProfileAdmin)
