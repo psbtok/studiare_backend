e21c52a340d34e36a8b004d8ac59b159cfce994d
Pete <opletaevpetr@mail.ru>
2024-10-22 22:44:24 +0300

    Rename api_service to user_service

--- user_service/apps.py
+++ user_service/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ApiServiceConfig(AppConfig):
+class UserServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api_service'
+    name = 'user_service'
