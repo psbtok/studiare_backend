39b8eacacfb00258ce07611b5158c9daac1de952
Pete <opletaevpetr@mail.ru>
2024-11-29 15:21:43 +0300

    Lesson Filtering

--- lesson_service/serializers.py
+++ lesson_service/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Lesson
+from user_service.serializers import UserSerializer
 
 class LessonSerializer(serializers.ModelSerializer):
+    student = UserSerializer(read_only=True)  
+
     class Meta:
         model = Lesson
         fields = '__all__'
--- lesson_service/views.py
+++ lesson_service/views.py
@@ -1,24 +1,41 @@
 from rest_framework import viewsets, permissions, filters
 from rest_framework.pagination import LimitOffsetPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as django_filters
 from .models import Lesson
 from .serializers import LessonSerializer
+from django.shortcuts import get_object_or_404
+from user_service.models import User
+from django.utils.timezone import make_aware
+from datetime import datetime
 
 class LessonPagination(LimitOffsetPagination):
     default_limit = 10
-    max_limit = 100    
+    max_limit = 100
+
+class LessonFilter(django_filters.FilterSet):
+    date_start_from = django_filters.DateTimeFilter(
+        field_name='date_start', 
+        lookup_expr='gte', 
+        label='Start from date'
+    )
+
+    class Meta:
+        model = Lesson
+        fields = ['tutor', 'date_start_from', 'date_end', 'subject', 'isConfirmed']
 
 class LessonViewSet(viewsets.ModelViewSet):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = [permissions.IsAuthenticated]
     pagination_class = LessonPagination  
-    filter_backends = [filters.OrderingFilter] 
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = LessonFilter
     ordering_fields = ['date_start', 'date_end']
     ordering = ['-date_start']
 
     def perform_create(self, serializer):
         user = self.request.user
-        serializer.save(tutor=user)
-
-    def perform_update_start(self, serializer):
-        serializer.save()
+        student_id = self.request.data.get('student')
+        student = get_object_or_404(User, pk=student_id)
+        serializer.save(tutor=user, student=student)
